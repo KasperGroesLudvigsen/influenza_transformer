4108d41e971a50bf50e1d01ef042a646a9887e91
Kasper Groes Albin Ludvigsen <37772085+KasperGroesLudvigsen@users.noreply.github.com>
2022-06-14 10:37:38 +0200

    Add files via upload

---
+++ positional_encoder.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn 
+import math
+from torch import nn, Tensor
+
+class PositionalEncoder(nn.Module):
+    """
+    The authors of the original transformer paper describe very succinctly what 
+    the positional encoding layer does and why it is needed:
+    
+    "Since our model contains no recurrence and no convolution, in order for the 
+    model to make use of the order of the sequence, we must inject some 
+    information about the relative or absolute position of the tokens in the 
+    sequence." (Vaswani et al, 2017)
+
+    Adapted from: 
+    https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+    https://github.com/LiamMaclean216/Pytorch-Transfomer/blob/master/utils.py 
+    """
+
+    def __init__(self, dropout: float = 0.1, max_seq_len: int = 5000, d_model: int = 512):
+
+        """
+        Parameters:
+
+            dropout: the dropout rate
+
+            max_seq_len: the maximum length of the input sequences
+
+            d_model: The dimension of the output of sub-layers in the model 
+                     (Vaswani et al, 2017)
+        """
+
+        super().__init__()
+
+        self.d_model = d_model
+        
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Create constant positional encoding matrix with values 
+        # dependent on position and i
+        position = torch.arange(max_seq_len).unsqueeze(1)
+        
+        exp_input = torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
+        
+        div_term = torch.exp(exp_input) # Returns a new tensor with the exponential of the elements of exp_input
+        
+        pe = torch.zeros(max_seq_len, d_model)
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        
+        pe[:, 1::2] = torch.cos(position * div_term) # torch.Size([58, 512])
+
+        pe = pe.unsqueeze(0).transpose(0, 1) # torch.Size([58, 1, 512])
+
+        # register that pe is not a model parameter
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [batch_size, enc_seq_len, dim_val]
+        """
+        print("From PositionalEncoding.forward(): size of input (x) = {}".format(x.size()))
+        add = self.pe[:x.size(1), :].squeeze(1)
+        print("From PositionalEncoding.forward(): size of add = {}".format(add.size()))
+        x = x + add
+
+        return self.dropout(x)
